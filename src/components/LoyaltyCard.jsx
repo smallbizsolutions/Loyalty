2eac18cc4267e9ab80b558f23bc1a51c3e37b84d
smallbizsolutions <austinrnorthrop@gmail.com>
2025-10-23 16:52:06 -0400

    Delete src/components directory

--- src/components/LoyaltyCard.jsx
+++
@@ -1,8 +0,0 @@
-export default function LoyaltyCard({ loyaltyId, points }) {
-  return (
-    <div className="loyalty-card">
-      <p>Your Loyalty ID: <strong>{loyaltyId}</strong></p>
-      <p>Points: {points}</p>
-    </div>
-  );
-}
