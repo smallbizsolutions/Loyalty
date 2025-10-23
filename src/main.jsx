1ca36ee46ec050fda20ee571c01d1fade39889df
smallbizsolutions <austinrnorthrop@gmail.com>
2025-10-23 11:21:25 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/widget.css";
 
+// --- FIX STARTS HERE ---
 const urlParams = new URLSearchParams(window.location.search);
-const businessId = urlParams.get("businessId");
+let businessId = urlParams.get("businessId");
+
+// If no businessId provided in URL, use a default (your test business)
+if (!businessId) {
+  businessId = "abc123"; // <-- change this if you want to test another business
+  console.warn("No businessId found in URL. Using default:", businessId);
+}
+// --- FIX ENDS HERE ---
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <App businessId={businessId} />
+  <React.StrictMode>
+    <App businessId={businessId} />
+  </React.StrictMode>
 );
